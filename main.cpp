ff53742b3bb1b1f209db7ab6409c38b86978ae92
Da Li <njulida@outlook.com>
2020-03-06 23:48:08 +0800

    client part via socket (low

---
+++ main.cpp
@@ -0,0 +1,44 @@
+//
+// Created by Da Li on 2020/3/6.
+//
+// Client side C/C++ program to demonstrate Socket programming
+#include <cstdio>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+#define PORT 4444
+
+int main(int argc, char const *argv[])
+{
+    int sock = 0;
+    struct sockaddr_in server_addr{};
+    char *hello = "Hello from client";
+    char buffer[1024] = {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    send(sock , hello , strlen(hello) , 0 );
+    printf("Hello message sent\n");
+    read(sock, buffer, 1024);
+    printf("%s\n",buffer );
+    return 0;
+}
