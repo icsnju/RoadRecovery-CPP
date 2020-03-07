05c0e4dea54eea1d4211c243997a531c8a7ff602
Da Li <njulida@outlook.com>
2020-03-07 21:00:48 +0800

    transfer to repo plibsys.

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-rm libmain.dylib libmain.so main.o
+#clean file
+rm main.o RoadRecovery-1.1.0.dll a.exe
 
-g++ -c -fPIC -o main.o main.cpp
-g++ -shared -o libRoadRecovery-1.1.0.so main.o
+#build dll file
+i686-w64-mingw32-g++ -c main.cpp
+#with compilation option -lws2_32 to use winsock
+i686-w64-mingw32-g++ -shared -o RoadRecovery-1.1.0.dll main.o -lws2_32 -lmswsock -ladvApi32
--- main.cpp
+++ main.cpp
@@ -2,61 +2,129 @@
 // Created by Da Li on 2020/3/6.
 //
 
-#include <cstdio>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <cstring>
+#define WIN32_LEAN_AND_MEAN
 
-#define PORT 4444
-#define default_buffer_size 10000
-char returnedJSONData[default_buffer_size] = {0};
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-extern "C" char* pathRestorationMethod(const char *JSONData) {
 
-    int sock = 0;
-    struct sockaddr_in server_addr;
+// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("\n Socket creation error \n");
-        return NULL;
+
+#define DEFAULT_BUFLEN 10000
+#define DEFAULT_PORT "4444"
+
+int __cdecl main(int argc, char *argv[])
+{
+    WSADATA wsaData;
+    SOCKET ConnectSocket = INVALID_SOCKET;
+    struct addrinfo *result = NULL,
+            *ptr = NULL,
+            hints;
+    const char *sendbuf = "this is a test";
+    char recvbuf[DEFAULT_BUFLEN];
+    int iResult;
+    int recvbuflen = DEFAULT_BUFLEN;
+
+    // Validate the parameters
+    if (argc != 2) {
+        printf("usage: provide an argument\n"
+               "\targv[1]: JSONFormat string.\n");
+        return 1;
     }
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0)
-    {
-        printf("\nInvalid address/ Address not supported \n");
-        return NULL;
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed with error: %d\n", iResult);
+        return 1;
     }
 
-    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-    {
-        printf("\nConnection Failed \n");
-        return NULL;
+    ZeroMemory( &hints, sizeof(hints) );
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    // Resolve the server address and port
+    iResult = getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result);
+    if ( iResult != 0 ) {
+        printf("getaddrinfo failed with error: %d\n", iResult);
+        WSACleanup();
+        return 1;
     }
 
-    send(sock, JSONData, strlen(JSONData), 0);
-    printf("Hello message sent\n");
-    read(sock, returnedJSONData, default_buffer_size);
-    printf("%s\n", returnedJSONData);
+    // Attempt to connect to an address until one succeeds
+    for(ptr=result; ptr != NULL ;ptr=ptr->ai_next) {
 
-    return returnedJSONData;
+        // Create a SOCKET for connecting to server
+        ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,
+                               ptr->ai_protocol);
+        if (ConnectSocket == INVALID_SOCKET) {
+            printf("socket failed with error: %ld\n", WSAGetLastError());
+            WSACleanup();
+            return 1;
+        }
+
+        // Connect to server.
+        iResult = connect( ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+        if (iResult == SOCKET_ERROR) {
+            closesocket(ConnectSocket);
+            ConnectSocket = INVALID_SOCKET;
+            continue;
+        }
+        break;
+    }
+
+    freeaddrinfo(result);
+
+    if (ConnectSocket == INVALID_SOCKET) {
+        printf("Unable to connect to server!\n");
+        WSACleanup();
+        return 1;
+    }
+
+    // Send an initial buffer
+    iResult = send( ConnectSocket, sendbuf, (int)strlen(sendbuf), 0 );
+    if (iResult == SOCKET_ERROR) {
+        printf("send failed with error: %d\n", WSAGetLastError());
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    printf("Bytes Sent: %ld\n", iResult);
+
+    // shutdown the connection since no more data will be sent
+    iResult = shutdown(ConnectSocket, SD_SEND);
+    if (iResult == SOCKET_ERROR) {
+        printf("shutdown failed with error: %d\n", WSAGetLastError());
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    // Receive until the peer closes the connection
+    do {
+
+        iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+        if ( iResult > 0 )
+            printf("Bytes received: %d\n", iResult);
+        else if ( iResult == 0 )
+            printf("Connection closed\n");
+        else
+            printf("recv failed with error: %d\n", WSAGetLastError());
+
+    } while( iResult > 0 );
+
+    // cleanup
+    closesocket(ConnectSocket);
+    WSACleanup();
+
+    return 0;
 }
-
-//int main(int argc, char const *argv[])
-//{
-//    if (argc != 2) {
-//        printf("Usage: please provide one argument\n"
-//               "\targv[1]: a JSON-format data (char *)"
-//               "\n");
-//        return -1;
-//    }
-//
-//    pathRestorationMethod(argv[1]);
-//
-//    return 0;
-//}
